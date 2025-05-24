b173a6aea8715cc5abce1291977d05b97dec40ad
Vivek Sharma <65083210+VvkSha@users.noreply.github.com>
2025-05-24 20:04:01 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (76.84%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,8 +2,15 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return maxLoot(nums, n, dp);
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int i=2; i<=n; i++){
+            dp[i] = Math.max(nums[i-1]+dp[i-2], dp[i-1]);
+        }
+
+        return dp[n];
+        // Arrays.fill(dp,-1);
+        // return maxLoot(nums, n, dp);
     }
 
     int maxLoot(int[] nums, int n, int[] dp){
