8607b5361bfeb14f80db240987691def2573bb5c
Vivek Sharma <65083210+VvkSha@users.noreply.github.com>
2025-05-25 21:22:15 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (81.51%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,24 +1,31 @@
 class Solution {
     public int climbStairs(int n) {
         //n=3  1,1,1,1,2
-        //[0,1,1,2]
-        if(n<=1) return 1;
-        if(n==2) return 2;
-        int[] dp = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
+        //[1,1,2,2]
+         if(n<=1) return 1;
+         if(n==2) return 2;
+        // int[] dp = new int[n+1];
+        // dp[0] = 1;
+        // dp[1] = 1;
 
-
+        int first = 1;
+        int sec = 1;
+        int cur = 0;
         for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
-
+            cur = first + sec ;
+            first =  sec;
+            sec = cur;
         }
+        return cur;
+        // for(int i=2; i<=n; i++){
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
 
 
         
        // Arrays.fill(dp,-1);
 
-        return dp[n];
+        //return dp[n];
     }
 
     int ways(int n, int[] dp){
