ab4e7bde0b5ed64bd7b567fb5cbe3ffe12ea88ee
Vivek Sharma <65083210+VvkSha@users.noreply.github.com>
2025-06-26 23:46:45 +0530

    Time: 527 ms (93.21%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,4 +1,7 @@
 /* Write your PL/SQL query statement below */
 
-select name as Customers from Customers c 
-left join Orders o on o.customerId  = c.id where o.customerId is null;
+-- select name as Customers from Customers c 
+-- left join Orders o on o.customerId  = c.id where o.customerId is null;
+
+select name as Customers from Customers c
+where not exists (select 1 from Orders o where o.customerId = c.id)
