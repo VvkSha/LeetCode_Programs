c3678cdad5d5953eec5535b75cd1e674c00d085a
Vivek Sharma <65083210+VvkSha@users.noreply.github.com>
2025-08-20 13:41:03 +0530

    Time: 921 ms (86.92%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,5 +1,5 @@
 /* Write your PL/SQL query statement below */
 
 select empu.unique_id, emp.name
-from Employees emp left outer join EmployeeUNI empu
+from Employees emp left join EmployeeUNI empu
 on emp.id = empu.id
