4779a54170747c11b0219c1cf682abf8d0a9cb1f
Vivek Sharma <65083210+VvkSha@users.noreply.github.com>
2025-08-20 18:32:44 +0530

    Time: 1024 ms (5.30%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -4,3 +4,7 @@
 select w1.id from weather w1 join weather w2
 on w1.recordDate = w2.recordDate + 1
 where w1.temperature > w2.temperature;
+-- select temp_table.id as 'Id' from(
+-- select id, temperature, LAG(temperature) over (order by recordDate) as prev_temp
+-- from weather
+-- ) temp_table where temperature > prev_temp;
